4f642d789b048d5c83efe15c4cfa1a968a7270e8
welington <welington.a2you@gmail.com>
2021-10-05 02:26:53 -0300

    criada pagina de login e senha

---
+++ hello.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="src/media/hello.css">
+	<link rel="stylesheet" type="text/css" href="src/media/reset.css">
+	<title>Hello </title>
+</head>
+<body>
+	<header>
+		
+			<h1>Desafio Hello</h1>
+
+	</header>
+	<main>
+			<section class="login">
+			
+				<form>
+					
+					<input class="input-login" type="text" name="login" id="login" placeholder="Retr0" required>
+					
+					<input class="input-login" type="password" name="pass" id="pass" placeholder="**************" required>
+				
+					<input class="enviar-login" type="submit" value="Login">
+					<input class="logout" type="submit" value="Logout">
+				</form>
+			</section>
+	</main>
+</body>
+</html>
