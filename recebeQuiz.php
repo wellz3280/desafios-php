1a005d976ccb367b96600930a3512cbc32fa5103
weliton <welingtonzsilva@gmail.com>
2021-10-12 01:23:33 -0300

    testes de funcionalidade

--- recebeQuiz.php
+++ recebeQuiz.php
@@ -1,10 +1,13 @@
 <?php
 	
 	 require_once 'src/Quiz/Quiz.php';
-            require_once 'src/Quiz/Pergunta1.php';
+            require_once 'src/Quiz/Perguntas.php';
 
-            $quiz1 = new Pergunta1($_GET['quiz']);
+            $quiz1 = new Perguntas('Qual o Maior País da América do Sul ?','brasil','brasil');
 
-            echo $quiz1->recuperaResposta();
-          
-             echo $quiz1->opcaoCorreta();
+            echo $quiz1->recuperaPergunta() . PHP_EOL;
+            echo PHP_EOL ."Resposta usuario: ".$quiz1->recuperaRespostaUsuario() . PHP_EOL;
+            echo PHP_EOL ."Resposta Correta : ".$quiz1->recuperaAlternativaCorreta() . PHP_EOL;
+          	
+          	$quiz1->Pontuacao();
+             //echo $quiz1->opcaoCorreta();
